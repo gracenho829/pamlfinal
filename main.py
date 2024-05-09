83a4078c7b868982f990c29929d07ad38b440721
gracenho829 <gracenho829@gmail.com>
2024-05-09 14:49:20 -0400

    changed to 2 columns

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ st.write("Our app will help you detect any fraudulent transactions!")
 st.divider()
 
 container = st.container(border=True)
-col1, col2, col3 = st.columns(3)
+col1, col2= st.columns(2)
 with container:
    st.write("Click one of these buttons to check for fraudulent transactions")
    with col1:
       st.button("Check 'Card 1' for Fraudulent Transactions", key="Card1", on_click=None)
    with col2:
       st.button("Check 'Card 2' for Fraudulent Transactions", key="Card2", on_click=None)
-   with col3:
-      st.button("Check 'Card 3' for Fraudulent Transactions", key="Card3", on_click=None)
+
+
+
+
 
