865ba4c205f3d9599a3b060d282a55674d60a32f
gracenho829 <gracenho829@gmail.com>
2024-05-12 21:18:43 -0400

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pickle
 import streamlit as st
 import pandas as pd
+import sklearn
 from sklearn.model_selection import train_test_split
 def preprocess_data():
    df = pd.read_csv('creditcard_2023.csv')
@@ -50,26 +51,47 @@ def random_forest(X,y):
 X,y = preprocess_data()
 decision_tree_pred = decision_tree(X, y)
 gradient_boosting_pred = gradient_boosting(X, y)
-# random_forest_pred = random_forest(X,y)
+random_forest_pred = random_forest(X,y)
 
-def run_models():
-   print("Decision_tree_pred", decision_tree_pred)
-   print("Gradient_Boosting_pred", gradient_boosting_pred)
-   print(len(decision_tree_pred))
-   #print(random_forest_pred)
+def run_models(card_number):
+   card_decision_tree = decision_tree_pred[100*card_number: 100*(card_number+1)]
+   card_gradient_tree = gradient_boosting_pred[100*card_number: 100*(card_number+1)]
+   card_random_forest = random_forest_pred[100*card_number: 100*(card_number+1)]
+   zeroes_ones = [0,0]
+   res = [0,0]
+   for i in range(len(card_decision_tree)):
+      zeroes_ones[int(card_decision_tree[i])] += 1
+      zeroes_ones[int(card_gradient_tree[i])] += 1
+      zeroes_ones[int(card_random_forest[i])] += 1
+
+      if zeroes_ones[0] > zeroes_ones[1]:
+         res[0] += 1
+      else:
+         res[1] += 1
+
+
+   return res
+
 
 st.title('Credit Card Fraud Detection')
 st.write("Our app will help you detect any fraudulent transactions!")
 st.divider()
 
 container = st.container(border=True)
+fraud, normal = "", ""
 with container:
    st.write("Click one of these buttons to check for fraudulent transactions")
    col1, col2 = st.columns(2)
    with col1:
       if st.button("Check 'Card 1' for Fraudulent Transactions", key="Card1"):
-         print("printed!")
+         card1 = run_models(0)
+         fraud = str(card1[0])
+         normal = str(card1[1])
    with col2:
       if st.button("Check 'Card 2' for Fraudulent Transactions", key="Card2"):
-         print("printed! 2")
+         card2 = run_models(1)
+         fraud = str(card2[0])
+         normal = str(card2[1])
+
+  st.write("You have " + fraud + " fraudulent transactions and " + normal + " normal transactions")
 
